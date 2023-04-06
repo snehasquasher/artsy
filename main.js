4993ac524dd2c61176e231858aa7b38437d262c9
Sneha <sneha.squasher@gmail.com>
2023-04-06 10:23:30 -0400

    added main.js file

---
+++ main.js
@@ -0,0 +1,5 @@
+
+    <body>
+        <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="main.js" type="text/javascript"></script>
+    </body>
