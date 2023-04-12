a59d4e3d7b0c87d60ade0a70786a745e83bf88d4
Sneha <sneha.squasher@gmail.com>
2023-04-11 22:12:28 -0400

    created landing + tutorial screens

---
+++ landing_screen.js
@@ -0,0 +1,5 @@
+const nextScreenBtn = document.getElementById('tutorial-screen-btn');
+
+nextScreenBtn.addEventListener('click', () => {
+  window.location.href = 'tutorial_screen.html';
+});
---
+++ tutorial_screen.js
@@ -0,0 +1,5 @@
+const nextScreenBtn = document.getElementById('next-screen-btn');
+
+nextScreenBtn.addEventListener('click', () => {
+  window.location.href = 'next-screen.html';
+});
