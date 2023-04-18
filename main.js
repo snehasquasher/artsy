0fb27676d6423c46bdf4133b97b3e3c6d2388a6b
Sneha <sneha.squasher@gmail.com>
2023-04-18 15:03:01 -0400

    removed twod

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ $(document).ready(function () {
 
 var frames = {
     socket: null,
-
     start: function () {
         var url = "ws://" + host + "/frames";
         frames.socket = new WebSocket(url);
@@ -17,32 +16,9 @@ var frames = {
             frames.show(JSON.parse(event.data));
         }
     },
-
     show: function (frame) {
         console.log(frame);
     }
-};
-
-var twod = {
-    socket: null,
-
-    // create a connection to the camera feed
-    start: function () {
-        var url = "ws://" + host + "/twod";
-        twod.socket = new WebSocket(url);
-
-        // whenever a new frame is received...
-        twod.socket.onmessage = function (event) {
-
-            // parse and show the raw data
-            twod.show(JSON.parse(event.data));
-        }
-    },
-
-    // show the image by adjusting the source attribute of the HTML img object previously created
-    show: function (twod) {
-        $('img.twod').attr("src", 'data:image/pnjpegg;base64,' + twod.src);
-    },
 };
 
  
