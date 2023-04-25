f3f4ae0b51ffa16faae9f06b069db810e6825124
Sneha <sneha.squasher@gmail.com>
2023-04-25 17:54:55 -0400

    ASIFFF changes

--- barcode.js
+++ barcode.js
@@ -1,20 +1,25 @@
 var qrCodeContainer = document.getElementById('qrCodeContainer');
-downloadUrl = localStorage.getItem('downloadUrl');
+var downloadUrl = localStorage.getItem('downloadUrl');
 
 var qrCode = new QRCode(qrCodeContainer, {
-	text: downloadUrl || 'https://www.google.com',
-	width: 256,
-	height: 256,
-	colorDark: 'darkblue',
-	colorLight: 'lightblue',
-	correctLevel: QRCode.CorrectLevel.H,
+    text: downloadUrl || 'https://www.google.com',
+    width: 256,
+    height: 256,
+    colorDark: 'darkblue',
+    colorLight: 'lightblue',
+    correctLevel: QRCode.CorrectLevel.H,
 });
-
 
 qrCodeContainer.style.display = 'block';
 qrCodeContainer.style.margin = 'auto';
 
-var button = document.getElementById('button');
-button.addEventListener('click', function () {
-	window.location.href = 'AddToHomeScreen.html';
-});
+var countdownElement = document.getElementById("barcode_countdown");
+var countdownValue = 20;
+var countdownInterval = setInterval(function() {
+    countdownValue--;
+    countdownElement.textContent = countdownValue;
+    if (countdownValue <= 0) {
+        clearInterval(countdownInterval);
+        window.location.href = 'index.html';
+    }
+}, 1000);
